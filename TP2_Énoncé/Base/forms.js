91b18cd709f60d03c290b4343d5fcdbf37037b8e
AdrienGrm <adrien.grimberg@polymtl.ca>
2023-02-06 19:03:23 -0500

    Ajout partie 2 js

---
+++ TP2_Énoncé/Base/forms.js
@@ -0,0 +1,4 @@
+let dateRapport = document.getElementById("add_date_incident");
+let boutonCreation = document.getElementById("add-submit");
+boutonCreation.setAttribute("disabled")
+console.log(dateRapport.textContent);
