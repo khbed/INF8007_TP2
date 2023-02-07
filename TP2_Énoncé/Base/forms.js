ef181d9773a13ab47a51ccddaf0710a399758a4c
AdrienGrm <adrien.grimberg@polymtl.ca>
2023-02-06 19:59:55 -0500

    Difficulté à faire une boucle ...

--- TP2_Énoncé/Base/forms.js
+++ TP2_Énoncé/Base/forms.js
@@ -1,4 +1,16 @@
 let dateRapport = document.getElementById("add_date_incident");
 let boutonCreation = document.getElementById("add-submit");
-boutonCreation.setAttribute("disabled")
-console.log(dateRapport.textContent);
+
+boutonCreation.setAttribute("disabled", true);
+
+function majBouton(date, bouton) {
+    if(bouton.disabled && date.value != ''){
+        bouton.deleteAttribute("disabled");
+    }
+    if(bouton.disabled==false && date.value == ''){
+        bouton.setAttribute("disabled", true);
+    }
+    console.log("test")
+}
+
+window.setInterval(majBouton(dateRapport, boutonCreation), 1000);
